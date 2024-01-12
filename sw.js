cc8d1056fdbf5d6719a3d10a794935bddea8089a
BenjamimFG <Ben.FreitasG@gmail.com>
2024-01-12 12:18:37 -0300

    add service worker

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
 // sw.js
+importScripts("https://progressier.app/kvAzzkNLLnorh9SdLCxp/sw.js");
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('app-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json', // Adicione todos os recursos que você deseja armazenar em cache
-          // Adicione mais recursos aqui conforme necessário
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open("app-cache").then((cache) => {
+//       return cache.addAll([
+//         "/",
+//         "/index.html",
+//         "/manifest.json", // Adicione todos os recursos que você deseja armazenar em cache
+//         // Adicione mais recursos aqui conforme necessário
+//       ]);
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
